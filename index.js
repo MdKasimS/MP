de45efc737dfe6ef77ed0935ab249150af14203d
Kasim Sache <itskasimsache@gmail.com>
2022-05-16 00:57:35 +0530

    Added Prod search page and working on Monog

---
+++ index.js
@@ -0,0 +1,29 @@
+const express= require('express');
+const path = require('path');
+
+// const connect = mongoose.connect(//)
+
+const publicPath = path.join(__dirname,'public');
+const app = express();
+
+// console.log(publicPath);
+
+app.use(express.static(publicPath));
+// app.use(express.static());
+
+app.get('/', (_, res)=>{
+    res.sendFile(`${__dirname}/index.html`);
+    console.log('Page requested');
+});
+
+app.get('/contact', (_, res)=>{
+    res.send('Error Page Not Found');
+});
+
+app.get('/books', (_, res)=>{
+    res.sendFile(`${publicPath}/product_search.html`)
+});
+
+
+console.log("Listening port 5000");
+app.listen(5000);
