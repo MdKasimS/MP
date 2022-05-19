0812baf0e705e15caaf815040b162381c8e4a737
Kasim Sache <itskasimsache@gmail.com>
2022-05-19 19:55:52 +0530

    Finally learned MongoDb and asyn await

---
+++ db/mongodb.js
@@ -0,0 +1,25 @@
+// get process's driver code
+const mongo = require('mongodb');
+
+// get process's ID (either finr or its predecided)
+const mongoClient = new mongo.MongoClient("mongodb://localhost:27017")
+
+
+const databaseName = 'local';
+const collectionName = 'via_csv';
+
+const connectDB = async ()=>{
+    
+    // get process's objects called connection object
+    let conObj = await mongoClient.connect();
+    
+    // call process's higher-level objects
+    let database = conObj.db(databaseName);
+    
+    // call process's lower-level objects
+    let collection = database.collection(collectionName);
+
+    return collection;
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,87 +1,31 @@
-console.log("Salam Hindutshan !!!");
+const dbConnect = require('./db/mongodb');
 
+
+console.log("Salam Hindutshan !!!");
 console.log("Alhamdulillah !!! All set !!!");
 
-// /* ---------- tried to do a 5 step driver-solution code------------
 
-// get process's driver code
-const mongo = require('mongodb');
-
-// get process's ID (either finr or its predecided)
-const mongoClient = new mongo.MongoClient("mongodb://localhost:27017")
-
-
-// get process's objects called connection object
-// call process's higher-level objects
-// call process's lower-level objects
-// const connectionObject = async function () {
-//     return await mongoClient.connect().then();
-// };
-
-// const database = async function () {
-//     return await connectionObject().then();//.db('local');
-
-// };
-
-// const myDb = async function () {
-//     let dbs = (await database()).db('local');
-//     return  dbs.collection('via_csv');
+// async function getData() {
+//     let collection = await dbConnect();
+//     let data = await collection.find().toArray();
+//     console.log(data)
 // }
 
-// const myCollection = async function(){
-//     let data = await myDb().then();
-//     console.log(await data.find().toArray());
-// };
+function setData() {}
+function updateData() {}
+function deleteData() {}
 
 
-const connectionObject = async ()=>{
-    return await mongoClient.connect();
-}
+const main = async ()=>{
+    console.log("main function");
+    let dbs = await dbConnect();
+    let colle = await dbs.find().toArray();
+    console.log(colle);
+};
 
-const database = async ()=>{
-    let dbs = await connectionObject();
-    let dbms = dbs.db('local');
-    let collection = dbms.collection('via_csv');
-    console.log(await collection.find({}).toArray());
+main();
 
-}
-
-
-function getData() {
-    // let connectionObject = await mongoClient.connect();
-    // console.log(connectionObject);
-
-    // let db = connectionObject.db('local');
-    // let collection = db.collection('via_csv');
-
-    // console.log(await collection.dbName); //not compulsory to write await here
-
-    // console.log(await collection.find({}).toArray());
-    // myCollection();
-    database();
-}
-
-
-async function setData() {
-    // let connectionObject = await mongoClient.connect();
-    // let db = await connectionObject.db('local');
-    // let collection = await db.collection('via_csv');
-    // console.log(await collection.stats()['wiredTiger']);
-}
-
-async function updateData() {
-    return 0;
-}
-
-async function deleteData() {
-    return 0;
-}
-
-
-getData();
-
-setData();
-
-updateData();
-
-deleteData();
+// getData();
+// setData();
+// updateData();
+// deleteData();
