eea1d46b999ee5dce152a00b360c23222fd53b82
Kasim Sache <itskasimsache@gmail.com>
2022-05-19 20:40:07 +0530

    Learning programmatic CRUD on MongoDB

--- server.js
+++ server.js
@@ -5,27 +5,36 @@ console.log("Salam Hindutshan !!!");
 console.log("Alhamdulillah !!! All set !!!");
 
 
-// async function getData() {
-//     let collection = await dbConnect();
-//     let data = await collection.find().toArray();
-//     console.log(data)
-// }
+async function getData() {
+    let collection = await dbConnect();
+    let data = await collection.find().toArray();
+    console.log(data)
+}
 
-function setData() {}
+async function setData() {
+    let collection = await dbConnect();
+    console.log("Alhamdulillah adding 1st data");
+    const status = await collection.bulkWrite(
+        {
+            insertOne:{
+                document:
+                {
+                    ISBN:"1623439671",
+                    JPEG:"1623439671.jpg",
+                    ImageURL:"http://ecx.images-amazon.com/images/I/61t-hrSw9BL.jpg",
+                    Title:"Doug the Pug 2016 Wall Calendar",
+                    Author:"Doug the Pug",
+                    Ratings:4,
+                }
+            }
+        });
+        console.log(status);
+};
+
 function updateData() {}
 function deleteData() {}
 
-
-const main = async ()=>{
-    console.log("main function");
-    let dbs = await dbConnect();
-    let colle = await dbs.find().toArray();
-    console.log(colle);
-};
-
-main();
-
-// getData();
+getData();
 // setData();
 // updateData();
 // deleteData();
