444bb98e91b2428986ab7a4f16ac5dbb09135391
Kasim Sache <itskasimsache@gmail.com>
2022-05-20 18:21:55 +0530

    working on getbooks

--- public/scripts/getBooks.js
+++ public/scripts/getBooks.js
@@ -1,3 +1,13 @@
 let counter = 0;
 let booksData = [];
 
+function getBooksData()
+{
+    console.log('Salam Hindusthan !!!');
+
+    let root = document.getElementsByClassName("row");
+    let sect = document.createElement("section")
+
+    root.append
+    console.log(root);
+}
